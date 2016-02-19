d0836c7b1c12f9df5a69085402a7fc872876d24d
RajuKoushik <g.rajukoushik@gmail.com>
2016-02-20 00:19:18 +0530

    Create automatic_login_after_wifi_connection.sh

---
+++ automatic_login_after_wifi_connection.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+IF=$1
+STATUS=$2
+
+if [ "$IF" == "IIITV1" ]
+then
+    case "$2" in
+        up)
+        logger -s "Login Script up triggered"
+        script=login.txt
+
+	username=your_username
+	password=your_password
+
+
+
+	echo 'key <tab>' >> $script
+
+
+	for i in `echo $username|fold -w1` 
+	do
+    		echo 'key '$i >> $script
+	done
+
+
+
+	echo 'key <tab>' >> $script
+
+
+	for i in `echo $password|fold -w1` 
+	do
+    		echo 'key '$i >> $script
+	done
+
+
+	echo 'key <tab>' >> $script
+	echo 'key ^J' >> $script
+	echo 'key Q' >> $script
+	echo 'key y' >> $script
+
+	lynx http://10.100.1.1:8090 -accept_all_cookies -cmd_script=login.txt
+        ;;
+        
+        *)
+        ;;
+    esac
+fi
